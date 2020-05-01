a7754fbdc1647d0a58dab08ab5c50634ce2298bf
Iqbal Maulana <iq.bluejack@gmail.com>
2020-05-01 19:45:32 +0700

    fix(rule): fix method getPresenceVerifierFor was renamed to getPresenceVerifier on laravel 7.x

--- src/Module/Rule/Validator/Validator.php
+++ src/Module/Rule/Validator/Validator.php
@@ -30,23 +30,23 @@ class Validator extends LaravelValidator
     {
         $this->requireParameterCount(1, $parameters, 'unique');
 
-        list($connection, $table) = $this->parseTable($parameters[0]);
+        [$connection, $table] = $this->parseTable($parameters[0]);
 
         // The second parameter position holds the name of the column that needs to
         // be verified as unique. If this parameter isn't specified we will just
         // assume that this column to be verified shares the attribute's name.
         $column = $this->getQueryColumn($parameters, $attribute);
 
-        list($idColumn, $id) = [null, null];
+        [$idColumn, $id] = [null, null];
 
         if (isset($parameters[2])) {
-            list($idColumn, $id) = $this->getUniqueIds($parameters);
+            [$idColumn, $id] = $this->getUniqueIds($parameters);
         }
 
         // The presence verifier is responsible for counting rows within this store
         // mechanism which might be a relational database or any other permanent
         // data store like Redis, etc. We will use it to determine uniqueness.
-        $verifier = $this->getPresenceVerifierFor($connection);
+        $verifier = $this->getPresenceVerifier($connection);
 
         $extra = $this->getUniqueExtra($parameters);
 
