2472ba9772a8f98deb672c65c78dc9f887350023
Iqbal Maulana <iq.bluejack@gmail.com>
2022-07-26 18:03:37 +0700

    feat(routing): add route commands
    Added route commands:
    - route:cache
    - route:clear
    - route:list

--- src/Bundle/RoutingBundle/RoutingBundle.php
+++ src/Bundle/RoutingBundle/RoutingBundle.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Pandawa\Bundle\RoutingBundle;
 
+use Illuminate\Console\Application as Artisan;
+use Illuminate\Foundation\Console\RouteCacheCommand;
+use Illuminate\Foundation\Console\RouteClearCommand;
+use Illuminate\Foundation\Console\RouteListCommand;
 use Pandawa\Bundle\DependencyInjectionBundle\Plugin\ImportServicesPlugin;
 use Pandawa\Component\Foundation\Bundle\Bundle;
 
@@ -12,6 +16,12 @@ use Pandawa\Component\Foundation\Bundle\Bundle;
  */
 class RoutingBundle extends Bundle
 {
+    protected array $commands = [
+        'RouteCache' => RouteCacheCommand::class,
+        'RouteClear' => RouteClearCommand::class,
+        'RouteList'  => RouteListCommand::class,
+    ];
+
     public function register(): void
     {
         $this->booted(function () {
@@ -20,6 +30,45 @@ class RoutingBundle extends Bundle
                     require $this->app->getCachedRoutesPath();
                 });
             }
+        });
+
+        $this->registerCommands();
+    }
+
+    protected function registerCommands(): void
+    {
+        foreach (array_keys($this->commands) as $command) {
+            $this->{"register{$command}Command"}();
+        }
+
+        $this->commands(array_values($this->commands));
+    }
+
+    protected function commands(array $commands): void
+    {
+        Artisan::starting(function ($artisan) use ($commands) {
+            $artisan->resolveCommands($commands);
+        });
+    }
+
+    protected function registerRouteCacheCommand(): void
+    {
+        $this->app->singleton(RouteCacheCommand::class, function ($app) {
+            return new RouteCacheCommand($app['files']);
+        });
+    }
+
+    protected function registerRouteClearCommand(): void
+    {
+        $this->app->singleton(RouteClearCommand::class, function ($app) {
+            return new RouteClearCommand($app['files']);
+        });
+    }
+
+    protected function registerRouteListCommand(): void
+    {
+        $this->app->singleton(RouteListCommand::class, function ($app) {
+            return new RouteListCommand($app['router']);
         });
     }
 
