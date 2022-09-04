deaa6f0985af185a35af9df852d3100dd7526462
Iqbal Maulana <iq.bluejack@gmail.com>
2022-09-04 23:22:09 +0700

    feat(config): add define variables feature for yaml

--- src/Component/Config/Loader/YamlLoader.php
+++ src/Component/Config/Loader/YamlLoader.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Pandawa\Component\Config\Loader;
 
+use Illuminate\Support\Arr;
 use Pandawa\Contracts\Config\LoaderInterface;
 use Pandawa\Contracts\Config\Parser\ParserResolverInterface;
 use Symfony\Component\Yaml\Yaml;
@@ -13,6 +14,8 @@ use Symfony\Component\Yaml\Yaml;
  */
 final class YamlLoader implements LoaderInterface
 {
+    const DEFINE_REGEX = '/\$\{([a-zA-Z0-9\.\-\_]+?)\}/';
+
     public function __construct(protected ParserResolverInterface $resolver)
     {
     }
@@ -23,6 +26,7 @@ final class YamlLoader implements LoaderInterface
 
         if ($parser = $this->resolver->resolve($config)) {
             $config = $parser->parse($config);
+            $config = $this->replaces(Arr::except($config, ['__defines']), $config['__defines'] ?? []);
         }
 
         return $config;
@@ -31,5 +35,32 @@ final class YamlLoader implements LoaderInterface
     public function supports(string $extension): bool
     {
         return in_array($extension, ['yaml', 'yml'], true);
+    }
+
+    protected function replaces(array $config, array $defines): array
+    {
+        if (empty($defines)) {
+            return $config;
+        }
+
+        return Arr::map($config, function (mixed $item) use ($defines) {
+            if (is_array($item)) {
+                return $this->replaces($item, $defines);
+            }
+
+            if (is_string($item) && preg_match_all(self::DEFINE_REGEX, $item, $matches)) {
+                foreach ($matches[1] as $i => $key) {
+                    if (empty($replacer = $defines[$key] ?? null)) {
+                        throw new \InvalidArgumentException(
+                            sprintf('Variable "%s" is not defined.', $matches[0][$i])
+                        );
+                    }
+
+                    $item = str_replace($matches[0][$i], $replacer, $item);
+                }
+            }
+
+            return $item;
+        });
     }
 }
