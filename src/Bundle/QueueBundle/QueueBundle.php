6d03c00b9af4b11eea881f5c6ff185bed1984e83
Iqbal Maulana <iq.bluejack@gmail.com>
2022-09-24 18:28:12 +0700

    feat(queue): register queue commands

--- src/Bundle/QueueBundle/QueueBundle.php
+++ src/Bundle/QueueBundle/QueueBundle.php
@@ -4,9 +4,25 @@ declare(strict_types=1);
 
 namespace Pandawa\Bundle\QueueBundle;
 
+use Illuminate\Console\Application as Artisan;
 use Illuminate\Contracts\Redis\Factory;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Queue\Connectors\NullConnector;
+use Illuminate\Queue\Console\BatchesTableCommand;
+use Illuminate\Queue\Console\ClearCommand as QueueClearCommand;
+use Illuminate\Queue\Console\FailedTableCommand;
+use Illuminate\Queue\Console\FlushFailedCommand as FlushFailedQueueCommand;
+use Illuminate\Queue\Console\ForgetFailedCommand as ForgetFailedQueueCommand;
+use Illuminate\Queue\Console\ListenCommand as QueueListenCommand;
+use Illuminate\Queue\Console\ListFailedCommand as ListFailedQueueCommand;
+use Illuminate\Queue\Console\MonitorCommand as QueueMonitorCommand;
+use Illuminate\Queue\Console\PruneBatchesCommand as QueuePruneBatchesCommand;
+use Illuminate\Queue\Console\PruneFailedJobsCommand as QueuePruneFailedJobsCommand;
+use Illuminate\Queue\Console\RestartCommand as QueueRestartCommand;
+use Illuminate\Queue\Console\RetryBatchCommand as QueueRetryBatchCommand;
+use Illuminate\Queue\Console\RetryCommand as QueueRetryCommand;
+use Illuminate\Queue\Console\TableCommand;
+use Illuminate\Queue\Console\WorkCommand as QueueWorkCommand;
 use Illuminate\Queue\QueueManager;
 use Illuminate\Queue\QueueServiceProvider;
 use Pandawa\Bundle\FoundationBundle\Plugin\ImportConfigurationPlugin;
@@ -29,6 +45,27 @@ class QueueBundle extends Bundle implements HasPluginInterface, DeferrableProvid
         'queue',
     ];
 
+    protected array $commands = [
+        'QueueClear'           => QueueClearCommand::class,
+        'QueueFailed'          => ListFailedQueueCommand::class,
+        'QueueFlush'           => FlushFailedQueueCommand::class,
+        'QueueForget'          => ForgetFailedQueueCommand::class,
+        'QueueListen'          => QueueListenCommand::class,
+        'QueueMonitor'         => QueueMonitorCommand::class,
+        'QueuePruneBatches'    => QueuePruneBatchesCommand::class,
+        'QueuePruneFailedJobs' => QueuePruneFailedJobsCommand::class,
+        'QueueRestart'         => QueueRestartCommand::class,
+        'QueueRetry'           => QueueRetryCommand::class,
+        'QueueRetryBatch'      => QueueRetryBatchCommand::class,
+        'QueueWork'            => QueueWorkCommand::class,
+    ];
+
+    protected array $devCommands = [
+        'QueueFailedTable'  => FailedTableCommand::class,
+        'QueueTable'        => TableCommand::class,
+        'QueueBatchesTable' => BatchesTableCommand::class,
+    ];
+
     public function configure(): void
     {
         $this->app->singleton('queue', function ($app) {
@@ -36,6 +73,8 @@ class QueueBundle extends Bundle implements HasPluginInterface, DeferrableProvid
                 $this->registerConnectors($manager);
             });
         });
+
+        $this->registerCommands();
     }
 
     public function plugins(): array
@@ -102,6 +141,115 @@ class QueueBundle extends Bundle implements HasPluginInterface, DeferrableProvid
     {
         $manager->addConnector('sqs', function () {
             return new SqsConnector();
+        });
+    }
+
+    protected function registerCommands(): void
+    {
+        foreach (array_keys([...$this->commands, ...$this->devCommands]) as $command) {
+            $this->{"register{$command}Command"}();
+        }
+
+        $this->commands(array_values($this->commands));
+    }
+
+    protected function commands(array $commands): void
+    {
+        Artisan::starting(function ($artisan) use ($commands) {
+            $artisan->resolveCommands($commands);
+        });
+    }
+
+    protected function registerQueueFailedCommand(): void
+    {
+        $this->app->singleton(ListFailedQueueCommand::class);
+    }
+
+    protected function registerQueueForgetCommand(): void
+    {
+        $this->app->singleton(ForgetFailedQueueCommand::class);
+    }
+
+    protected function registerQueueFlushCommand(): void
+    {
+        $this->app->singleton(FlushFailedQueueCommand::class);
+    }
+
+    protected function registerQueueListenCommand(): void
+    {
+        $this->app->singleton(QueueListenCommand::class, function ($app) {
+            return new QueueListenCommand($app['queue.listener']);
+        });
+    }
+
+    protected function registerQueueMonitorCommand(): void
+    {
+        $this->app->singleton(QueueMonitorCommand::class, function ($app) {
+            return new QueueMonitorCommand($app['queue'], $app['events']);
+        });
+    }
+
+    protected function registerQueuePruneBatchesCommand(): void
+    {
+        $this->app->singleton(QueuePruneBatchesCommand::class, function () {
+            return new QueuePruneBatchesCommand;
+        });
+    }
+
+    protected function registerQueuePruneFailedJobsCommand(): void
+    {
+        $this->app->singleton(QueuePruneFailedJobsCommand::class, function () {
+            return new QueuePruneFailedJobsCommand;
+        });
+    }
+
+    protected function registerQueueRestartCommand(): void
+    {
+        $this->app->singleton(QueueRestartCommand::class, function ($app) {
+            return new QueueRestartCommand($app['cache.store']);
+        });
+    }
+
+    protected function registerQueueRetryCommand(): void
+    {
+        $this->app->singleton(QueueRetryCommand::class);
+    }
+
+    protected function registerQueueRetryBatchCommand(): void
+    {
+        $this->app->singleton(QueueRetryBatchCommand::class);
+    }
+
+    protected function registerQueueWorkCommand(): void
+    {
+        $this->app->singleton(QueueWorkCommand::class, function ($app) {
+            return new QueueWorkCommand($app['queue.worker'], $app['cache.store']);
+        });
+    }
+
+    protected function registerQueueClearCommand(): void
+    {
+        $this->app->singleton(QueueClearCommand::class);
+    }
+
+    protected function registerQueueFailedTableCommand(): void
+    {
+        $this->app->singleton(FailedTableCommand::class, function ($app) {
+            return new FailedTableCommand($app['files'], $app['composer']);
+        });
+    }
+
+    protected function registerQueueTableCommand(): void
+    {
+        $this->app->singleton(TableCommand::class, function ($app) {
+            return new TableCommand($app['files'], $app['composer']);
+        });
+    }
+
+    protected function registerQueueBatchesTableCommand(): void
+    {
+        $this->app->singleton(BatchesTableCommand::class, function ($app) {
+            return new BatchesTableCommand($app['files'], $app['composer']);
         });
     }
 }
