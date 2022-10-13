22109127d2e8274546d94cd966fb4c66cd846549
Iqbal Maulana <iq.bluejack@gmail.com>
2022-10-14 00:02:36 +0700

    fix(fix): fix missing alias

--- src/Bundle/SessionBundle/SessionBundle.php
+++ src/Bundle/SessionBundle/SessionBundle.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Pandawa\Bundle\SessionBundle;
 
+use Illuminate\Contracts\Session\Session;
 use Illuminate\Cookie\CookieServiceProvider;
+use Illuminate\Session\SessionManager;
 use Illuminate\Session\SessionServiceProvider;
+use Illuminate\Session\Store;
 use Pandawa\Bundle\FoundationBundle\Plugin\ImportConfigurationPlugin;
 use Pandawa\Bundle\FoundationBundle\Plugin\RegisterBundlesPlugin;
 use Pandawa\Bundle\RoutingBundle\Plugin\ImportMiddlewareAnnotationPlugin;
@@ -17,6 +20,13 @@ use Pandawa\Contracts\Foundation\HasPluginInterface;
  */
 class SessionBundle extends Bundle implements HasPluginInterface
 {
+    public function configure(): void
+    {
+        $this->app->alias('session', SessionManager::class);
+        $this->app->alias('session.store', Store::class);
+        $this->app->alias('session.store', Session::class);
+    }
+
     public function plugins(): array
     {
         return [
